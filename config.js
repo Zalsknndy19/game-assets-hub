bc21ea567743133613b2823b1e654dffc8a51c43
Rizal Hakim <zalsknndy19@gmail.com>
2025-12-30 18:48:09 +0700

    First Deployment

---
+++ config.js
@@ -0,0 +1,8 @@
+// FILE PUSAT DAFTAR ANGGOTA & ID HISTATS
+var teamConfig = {
+    "Mjolnir": "4999294",
+    "Thor"   : "4999293",
+    "Loki"   : "4999295", // Anggota baru tinggal tambah di sini
+    "Hulk"   : "4999296",
+    "Owner"  : "4999292"
+};
